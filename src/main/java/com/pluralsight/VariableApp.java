0673f532d3501c5f8d61e818067aacd0aa1bf389
AliciaD02 <diazalicia4542@gmail.com>
2026-04-09 11:27:00 -0500

    First Commit

--- src/main/java/com/pluralsight/VariableApp.java
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -2,4 +2,76 @@ package com.pluralsight;
 
 public class VariableApp {
 
+    public static <Int> void main(String[] args) {
+
+        //1. Declare a variable to store your favorite color
+
+        String favColor = "black";
+
+        System.out.println("My favorite color is " + favColor);
+
+        //2. Declare a variable to store the year you started this class
+
+        int year = 2026;
+
+        System.out.println("The year I started is " + year);
+
+        //3. Declare a variable to store your middle initial
+
+        char middleInitial = 'H';
+        System.out.println("My middle name initial is "+ middleInitial);
+
+        //4. Declare a variable to store whether you have pets (yes/no)
+
+        boolean havePets = true;
+        System.out.println("Do you have a pet ?" + havePets);
+
+        //5. Declare a variable with a nice message or saying
+
+        String Message =  "Stay positive";
+        System.out.println("A nice message would be " + Message);
+
+        //Step 2
+
+        // The number of days in a week.
+
+        int daysInWeek = 7;
+        System.out.println( "There are " + daysInWeek + " days in a week.");
+
+        // The price of coffee ($4.99)
+        double coffeePrice = 4.99;
+        System.out.println("The price of coffee is $" + coffeePrice);
+
+        // Your favorite letter
+        char favoriteLetter = 'A';
+        System.out.println("My favorite letter is " + favoriteLetter);
+
+
+
+        // Whether it's raining(true/false)
+        boolean isRaining = true;
+        System.out.println("Is it raining? " + isRaining);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
